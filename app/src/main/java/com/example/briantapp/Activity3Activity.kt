7973fc7bc570267d4868a390b996d5d404125fd4
LATITUDE E5450 <fambabrian5@gmail.com>
2024-03-16 19:48:10 +0300

    first step done

---
+++ app/src/main/java/com/example/briantapp/Activity3Activity.kt
@@ -0,0 +1,16 @@
+package com.example.briantapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Activity3Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity3)
+
+        }
+    }
